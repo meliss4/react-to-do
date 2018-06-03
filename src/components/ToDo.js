a3d31c2c3a0b0522c94cc91d43f1fdf0179baf06
Melissa Riggs <melissarocksoc@gmail.com>
2018-06-03 13:44:14 -0700

    state and props checkpoint

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,9 +4,9 @@ class ToDo extends Component {
     render () {
         return (
             <li>
-                <input type="checkbox" checked={ this.props.isCompleted } />
-                <span> { this.props.description } </span>
-            </li>
+         <input type="checkbox" checked={ this.props.isCompleted } />
+         <span>{ this.props.description }</span>
+       </li>
         );
     }
 }
